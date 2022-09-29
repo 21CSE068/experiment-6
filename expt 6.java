64953aef2e83a1af112cdd9c238e739c14da7e9d
21CSE068 <111351992+21CSE068@users.noreply.github.com>
2022-09-29 22:11:01 +0530

    Update expt 6.java

--- expt 6.java
+++ expt 6.java
@@ -1,6 +1,6 @@
 public class ThrowUsedCarExceptions {
     public static void main(String[] args) {
-        UsedCar[] usedCars = new UsedCar[7];
+        UsedCar[] usedCars = new UsedCar[6];
                 try {
                         usedCars[0] = new Usedcar("3400","honda",1983,5000,22220);
                 }
@@ -34,12 +34,6 @@ public class ThrowUsedCarExceptions {
                 }
                 try {
                         usedCars[5] = new Usedcar("5000","other",2011,-60000,12000);
-                }
-                catch(UsedCarException e) {
-                        System.out.println(e.getMessage());
-                }
-                try {
-                        usedCars[6] = new Usedcar("0100","other",2000,15000,43000);
                 }
                 catch(UsedCarException e) {
                         System.out.println(e.getMessage());
